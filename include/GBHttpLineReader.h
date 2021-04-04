df41d9ac95aa7a001348a3e9aef7ba67e785ff82
jeonghunlee <ljhoo8@naver.com>
2021-04-04 21:20:38 +0900

    The parse function of GBHttpLineReader fixed.

--- include/GBHttpLineReader.h
+++ include/GBHttpLineReader.h
@@ -4,6 +4,16 @@ namespace GenericBoson
 {
 	class GBHttpLineReader
 	{
+		/*
+		Tokens parsed by Parse function.
+		*/
+		std::vector<std::string> m_parsed;
+
+		/*
+		Parse a line in the HTTP message.
+
+		\return Was It succeeded?
+		*/
 		bool Parse(const std::string_view);
 	public:
 		GBHttpLineReader() = default;
--- src/GBHttpLineReader.cpp
+++ src/GBHttpLineReader.cpp
@@ -3,8 +3,38 @@
 
 namespace GenericBoson
 {
-	bool GBHttpLineReader::Parse(const std::string_view)
+	bool GBHttpLineReader::Parse(const std::string_view target)
 	{
+		std::string token;
+
+		// All Http message ( except for Entity-Body ) must be ended by CRLF.
+		bool CRReadJustBefore = false;
+		for (auto iChar : target)
+		{
+			switch (iChar)
+			{
+			case '\r':
+				CRReadJustBefore = true;
+				break;
+			case '\n':
+				if (true == CRReadJustBefore)
+				{
+					token.push_back(iChar);
+					return true;
+				}
+				CRReadJustBefore = false;
+				break;
+			case ' ':
+				CRReadJustBefore = false;
+				m_parsed.push_back(token);
+				break;
+			default:
+				CRReadJustBefore = false;
+				token.push_back(iChar);
+				break;
+			}
+		}
+
 		return false;
 	}
 }
