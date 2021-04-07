bddf9ab25057eebd5d24119c0e648afd262e41aa
jeonghunlee <ljhoo8@naver.com>
2021-04-07 22:58:35 +0900

    1. add PathSegment.* files to filter "Router". 2. Implement GBPathSegment class a little bit.

--- include/GBPathSegment.h
+++ include/GBPathSegment.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <set>
+#include <variant>
+
 namespace GenericBoson
 {
+	//using METHOD = std::variant<GET,HEAD,POST>;
+
 	class PathSegment
 	{
+		std::set<PathSegment> m_subPath;
+
+		//std::vector<METHOD> m_methodArray;
 	};
 }
