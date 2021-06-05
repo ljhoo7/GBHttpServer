298ac8f00249a8e080797f92c759dd9a134b1312
jeonghunlee <ljhoo8@naver.com>
2021-06-05 23:28:57 +0900

    1. TestCase frame 2. Add dotnet restore script to .travis.yml

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestCaseBase focusedTestCase = new TestCase1();
         }
     }
 }
