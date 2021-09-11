a7664ae9ea1438aa8832416ce5794a9b3f7910e6
jeonghunlee <ljhoo8@naver.com>
2021-09-11 20:37:20 +0900

    Before & After

--- include/GBHttpStaticRouter.h
+++ include/GBHttpStaticRouter.h
@@ -14,39 +14,63 @@ namespace Proud
 	void Put(CALLABLE callable)
 	{
 		// 사전 작업을 여기서 한다.
+		static const char[] putStr = "Put";
+		Before<putStr, PATH>();
 
 		callable();
 
 		// 사후 작업을 여기서 한다.
+		After<putStr, PATH>();
 	}
 
 	template<const char* PATH, typename CALLABLE>
 	void Head(CALLABLE callable)
 	{
 		// 사전 작업을 여기서 한다.
+		static const char[] putStr = "Head";
+		Before<putStr, PATH>();
 
 		callable();
 
 		// 사후 작업을 여기서 한다.
+		After<putStr, PATH>();
 	}
 
 	template<const char* PATH, typename CALLABLE>
 	void Get(CALLABLE callable)
 	{
 		// 사전 작업을 여기서 한다.
+		static const char[] putStr = "Get";
+		Before<putStr, PATH>();
 
 		callable();
 
 		// 사후 작업을 여기서 한다.
+		After<putStr, PATH>();
 	}
 
 	template<const char* PATH, typename CALLABLE>
 	void Post(CALLABLE callable)
 	{
 		// 사전 작업을 여기서 한다.
+		static const char[] putStr = "Post";
+		Before<putStr, PATH>();
 
 		callable();
 
 		// 사후 작업을 여기서 한다.
+		After<putStr, PATH>();
+	}
+
+	template<const char* METHOD_NAME, const char* PATH>
+	void Before()
+	{
+
+	}
+
+	template<const char* METHOD_NAME, const char* PATH>
+	void After()
+	{
+
 	}
 }
