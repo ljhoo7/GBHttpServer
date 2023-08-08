dd06ed74926363ddf0f9213f4e5f7712f06a1a0e
jeonghunlee <ljhoo8@naver.com>
2023-08-08 23:20:08 +0900

    - abandon vcpkg - make my project use boost

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,7 +1,7 @@
 #include "../include/stdafx.h"
 #include "../include/GBHttpServer.h"
 
-#include <boost/thread/future.hpp>
+#include "boost/thread/future.hpp"
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -9,40 +9,42 @@ using namespace GenericBoson;
 
 int main()
 {
-	GBHttpServer srv;
-
-	//srv.GET("/test/test2", [](RequestQueryMap requestQueryMap, RequestHeaderMap requestHeaderMap, RequestBody requestBody)->GBHttpResponse
-	//	{
-	//		std::cout << "here !" << std::endl;
-
-	//		//requestBody.ReadFile();
-
-	//		GBHttpResponse response(HTTP_STATUS_CODE::OK);
-
-	//		//response.WriteFile();
-
-	//		return response;
-	//	});
-
-	//PUT("/test/test2", []()
-	//{
-	//	std::cout << "here !" << std::endl;
-
-	//	//requestBody.ReadFile();
-
-	//	//GBHttpResponse response(HTTP_STATUS_CODE::OK);
-
-	//	//response.WriteFile();
-
-	//	//return response;
-	//});
-
-	srv.Start();
-
-	while (true)
-	{
-		Sleep(100);
-	}
 	
-	return 0;
 }
+
+//GBHttpServer srv;
+
+//srv.Start();
+
+//while (true)
+//{
+//	Sleep(100);
+//}
+//
+//return 0;
+
+//srv.GET("/test/test2", [](RequestQueryMap requestQueryMap, RequestHeaderMap requestHeaderMap, RequestBody requestBody)->GBHttpResponse
+//	{
+//		std::cout << "here !" << std::endl;
+
+//		//requestBody.ReadFile();
+
+//		GBHttpResponse response(HTTP_STATUS_CODE::OK);
+
+//		//response.WriteFile();
+
+//		return response;
+//	});
+
+//PUT("/test/test2", []()
+//{
+//	std::cout << "here !" << std::endl;
+
+//	//requestBody.ReadFile();
+
+//	//GBHttpResponse response(HTTP_STATUS_CODE::OK);
+
+//	//response.WriteFile();
+
+//	//return response;
+//});
